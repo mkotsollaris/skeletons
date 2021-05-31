4822563cc6037d2a5b36040c8f52c71ca8f3a235
mkotsollaris <mkotsollari@gmail.com>
2021-05-30 22:36:26 -0700

    pushed reduce example

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,35 @@
 const hello = () => 'hello world!';
 
+const reducer = (accumulator, currentValue) => {
+    if (accumulator.length === 0) {
+        accumulator.push(currentValue);
+        return accumulator;
+    }
+
+    const index = accumulator.findIndex(el => el[1] === currentValue[1]);
+
+    if (index !== -1) {
+        accumulator[index][0] += currentValue[0];
+        return accumulator;
+    }
+    accumulator.push(currentValue);
+    return accumulator
+};
+
+let withForLoop = (input) => {
+    let res = [];
+    for (let i = 0; i < input.length; i++) {
+        let currInput = input[i];
+        const index = res.findIndex(el => el[1] === currInput[1]);
+        if (index !== -1) {
+            res[index][0] += currInput[0];
+            continue;
+        }
+        res.push(currInput);
+    }
+    return res;
+}
+
+export { reducer, withForLoop }
+
 export default hello;
--- javascript/index.test.js
+++ javascript/index.test.js
@@ -1,8 +1,35 @@
-import hello from ".";
+import hello, { withForLoop } from ".";
+import { reducer } from '.';
 
 describe('index', () => {
 
     it('hello world', () => {
         expect(hello()).toBe('hello world!');
+    })
+
+    it('reducer', () => {
+        var currInv = [
+            [21, "Bowling Ball"],
+            [2, "Dirty Sock"],
+            [1, "Hair Pin"],
+            [5, "Microphone"]
+        ];
+
+        var newInv = [
+            [2, "Hair Pin"],
+            [3, "Half-Eaten Apple"],
+            [67, "Bowling Ball"],
+            [7, "Toothpaste"]
+        ];
+
+        let invs = [...currInv, ...newInv];
+
+        const sortFn = (a, b) => a[0] - b[0];
+
+        let withForLoopRes = withForLoop([...invs]).sort(sortFn)
+        let withReducerRes = [...invs].reduce(reducer, []).sort(sortFn);
+
+        console.log('withForLoopRes', withForLoopRes);
+        console.log('withReducerRes', withReducerRes);
     })
 });
